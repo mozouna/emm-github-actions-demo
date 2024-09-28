770b63972c4993a63120d40ae5ad928c7f7f5501
El Mehdi Mozouna <elmozouna@RABWC2C5797G4.local>
2024-09-28 17:56:02 +0100

    first commit

---
+++ MyProject.API/Services/MathService.cs
@@ -0,0 +1,15 @@
+namespace MyProject.API.Services
+{
+    public class MathService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
